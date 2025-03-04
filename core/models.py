d6c18c9246710f0062f18e05260b3e8d07b902a2
doctormatterhorn <dmf030@gmail.com>
2025-03-04 20:46:59 -0300

    finished User model

--- core/models.py
+++ core/models.py
@@ -45,10 +45,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_invisible = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    # is_active = models.BooleanField(default=True)
-    # is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'crm'    
 
+    def __str__(self):
+        return self.name
+    
