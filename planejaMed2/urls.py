34bddfff130949e923f7b54853f4c2641ce338b8
dmfadul <dmf030@gmail.com>
2025-03-10 16:04:02 -0300

    made home redirect to dashboard

--- planejaMed2/urls.py
+++ planejaMed2/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from shifts.views import dashboard
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('shifts/', include('shifts.urls')),
+    path('', dashboard, name='home'),
 ]
