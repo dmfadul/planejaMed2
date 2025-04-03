d6a8df8adfcde99a1724171f8879cf3e966e2c88
dmfadul <dmf030@gmail.com>
2025-04-03 16:33:39 -0300

    added and registered a basetable page

--- shifts/urls.py
+++ shifts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, basetable
 
 app_name = "shifts"
 
 urlpatterns = [
     path("dashboard", dashboard, name="dashboard"),
+    path("basetable", basetable, name="basetable"),
 ]
--- shifts/views.py
+++ shifts/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+def basetable(request):
+    return render(request, "shifts/table.html")
+
+
 @login_required
 def dashboard(request):
     return render(request, "shifts/dashboard.html")
