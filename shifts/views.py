478a5f78aaf31d094ddff511c38ceff82832e4f8
doctormatterhorn <dmf030@gmail.com>
2025-04-03 23:11:24 -0300

    added css to table

--- shifts/views.py
+++ shifts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+import math
 
 
 def basetable(request):
+    header = [""] + [str(i) * 7 for i in range(1, 6)],
+    print(header)
     context = {
         "header1": [""] + ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"] * 5,
-        "header2": [""] + [str(i) * 7 for i in range(1, 6)],                           
+        "header2": [""] + [math.ceil(int(x)/7) for x in range(1, 36)],
     }
 
     return render(request, "shifts/table.html", context)
