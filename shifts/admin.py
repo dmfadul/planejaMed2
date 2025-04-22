a38b4eb31ea2506444e3b5edab81107d75820f6c
dmfadul <dmf030@gmail.com>
2025-04-22 09:46:06 -0300

    registered shift models to admin

--- shifts/admin.py
+++ shifts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Month, Holiday, Center
+from .models import Shift, TemplateShift 
 
-# Register your models here.
+
+admin.site.register(Month)
+admin.site.register(Holiday)
+admin.site.register(Center)
+admin.site.register(Shift)
+admin.site.register(TemplateShift)
