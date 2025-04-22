b06eb7ef1b45f3744dfd9c5c7d887a5e1eda1d81
dmfadul <dmf030@gmail.com>
2025-04-22 15:59:36 -0300

    added users to shifts table

--- shifts/views.py
+++ shifts/views.py
@@ -1,16 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from core.models import User
 import math
 
 
-def basetable(request, center):
-    print(center)
-    header = [""] + [str(i) * 7 for i in range(1, 6)],
-    print(header)
+def basetable(request, center):    
     context = {
         "header1": [""] + ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"] * 5,
         "header2": [""] + [math.ceil(int(x)/7) for x in range(1, 36)],
+        "table": []
     }
+
+    users = User.objects.filter(is_active=True, is_invisible=False).order_by("name")
+    for user in users:
+        line = [user.name] + [""] * (len(context["header1"]) - 1)
+        context["table"].append(line)
+
+    for line in context["table"]:
+        print(line)
 
     return render(request, "shifts/table.html", context)
 
