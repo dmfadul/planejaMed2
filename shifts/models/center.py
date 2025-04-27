27824f870d74fefe4d8ad0ec33bb16a66fb6c927
doctormatterhorn <dmf030@gmail.com>
2025-04-26 21:04:29 -0300

    made center abbreviation unique

--- shifts/models/center.py
+++ shifts/models/center.py
@@ -4,6 +4,7 @@ from core.models import User
 
 class Center(models.Model):
     name = models.CharField(max_length=100)
-    abbreviation = models.CharField(max_length=5)
+    abbreviation = models.CharField(max_length=5, unique=True)
     hospital = models.CharField(max_length=100, default='Evangélico')
     is_active = models.BooleanField(default=True)
+    
