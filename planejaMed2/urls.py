24e564f17aff3fa16839c9a7b4aacfcefe5d93d7
doctormatterhorn <dmf030@gmail.com>
2025-04-27 11:07:17 -0300

    removed 'core' from url

--- planejaMed2/urls.py
+++ planejaMed2/urls.py
@@ -21,7 +21,7 @@ from shifts.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
+    path('', include('core.urls')),
     path('shifts/', include('shifts.urls')),
     path('', dashboard, name='home'),
 ]
