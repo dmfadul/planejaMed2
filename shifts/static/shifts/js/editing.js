e7c7c7e471cf62e26437131b869d1100caffa8b8
doctormatterhorn <dmf030@gmail.com>
2025-04-27 01:59:50 -0300

    added js styling to control panel table buttons

--- shifts/static/shifts/js/editing.js
+++ shifts/static/shifts/js/editing.js
@@ -1,3 +1,44 @@
+let editing = false;
+
+let state = {
+    mode: null, // 'edit' or null
+    action: null, // 'add', 'remove' or null
+    center: null,
+    month: null,
+    year: null,
+    selectedCells: [],
+};
+
+function toggleEditing() {
+    editing = !editing;
+
+    const editButton = document.getElementById('edit-button');
+    const defaultButtons = document.getElementById('default-buttons');
+    const editButtons = document.getElementById('edit-buttons');
+
+    if (editing) {
+        editButton.textContent = "Finalizar";
+        editButton.classList.add("editing-active");
+        defaultButtons.style.display = "none";
+        editButtons.style.display = "flex"; // flex or block, depending on layout
+        startEditing();
+    } else {
+        editButton.textContent = "Editar";
+        editButton.classList.remove("editing-active");
+        defaultButtons.style.display = "flex";
+        editButtons.style.display = "none";
+        exitEditing();
+    }
+}
+
 function startEditing() {
     console.log("Editing started");
+    state.mode = "edit";
+    console.log(state);
+}
+
+function exitEditing(){
+    console.log("Editing exited");
+    state.mode = null;
+    console.log(state);
 }
