e9d75edd0e9e764632537cb6b1348d386d3ae297
doctormatterhorn <dmf030@gmail.com>
2025-04-27 18:36:03 -0300

    moved sendData and listeners to their own files

--- shifts/static/shifts/js/editing.js
+++ shifts/static/shifts/js/editing.js
@@ -97,37 +97,3 @@ function executeEdit(action) {
     getData();
     sendData();
 }
-
-
-function sendData() {
-    console.log(state);
-    console.log(state.selectedCells);
-    clearSelectedCells();
-}
-
-
-// Event listeners
-const cells = document.querySelectorAll('.cell-col');
-const names = document.querySelectorAll('.name-col');
-const headers = document.querySelectorAll('.header');
-const corners = document.querySelectorAll('.corner');
-
-const addButton = document.getElementById('edit-add-button');
-const remButton = document.getElementById('edit-rem-button');
-
-
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        clickCell(cell);
-    })
-});
-
-
-addButton.addEventListener('click', () => {
-    executeEdit('add');
-});
-
-
-remButton.addEventListener('click', () => {
-    executeEdit('remove');
-});
---
+++ shifts/static/shifts/js/sendData.js
@@ -0,0 +1,6 @@
+function sendData() {
+    console.log(state);
+    console.log(state.selectedCells);
+    clearSelectedCells();
+    console.log("Data sent to server");
+}
