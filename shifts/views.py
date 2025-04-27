97c2c8ad640f16cdb1c38e5fc0323312e9623c3c
doctormatterhorn <dmf030@gmail.com>
2025-04-26 22:11:49 -0300

    set up a css folder for the doctor basetable

--- shifts/views.py
+++ shifts/views.py
@@ -12,10 +12,10 @@ def basetable(request, center):
     context = {
         "center": "CCG",
         "table_type": "BASE",
+        "template": "basetable",
         "header1": [""] + weekdays * 5,
         "header2": [""] + indexes,
         "doctors": [],
-        "template": "basetable"
     }
 
     users = User.objects.filter(is_active=True, is_invisible=False).order_by("name")
@@ -27,9 +27,33 @@ def basetable(request, center):
 
     return render(request, "shifts/table.html", context)
 
+
 def doctor_basetable(request, center, crm):
-    print(center, crm)
-    return HttpResponse("Hello, world. You're at the doctor basetable page.")
+    weekdays = ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"]
+
+    user = User.objects.get(crm=crm)
+    shifts = {
+        weekdays[0]: [""] * 5,
+        weekdays[1]: [""] * 5,
+        weekdays[2]: [""] * 5,
+        weekdays[3]: [""] * 5,
+        weekdays[4]: [""] * 5,
+        weekdays[5]: [""] * 5,
+        weekdays[6]: [""] * 5,
+    }
+
+    context = {
+    "center": center,
+    "table_type": "BASE",
+    "template": "doctor_basetable",
+    "header1": [""] + [i for i in range(1, 6)],
+    "weekdays": weekdays,
+    "doctor": user,
+    "shifts": shifts.items(),
+    }
+
+    return render(request, "shifts/doctor_basetable.html", context)
+
 
 @login_required
 def dashboard(request):
