2e4a7b9da6a990466dd0f556a6dbcfcd845ebe74
dmfadul <dmf030@gmail.com>
2025-04-28 16:59:47 -0300

    finished basic fetch on sendData

--- shifts/static/shifts/js/sendData.js
+++ shifts/static/shifts/js/sendData.js
@@ -1,6 +1,53 @@
-async function sendData() {
-    
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.startsWith(name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
+
+async function sendData() {
+    try {
+        await confirmData();
+
+        const response = await fetch('/shifts/update', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken'),
+            },
+            body: JSON.stringify(state)
+        });
+
+        if (!response.ok) {
+            throw new Error(`Server responded with status ${response.status}`);
+        }
+
+        const data = await response.json(); // Expecting { updates: [{ cellId, newValue }, ...] }
+        //apply updates
+
+        console.log("Appointments updated succefully!");
+
+    } catch (error) {
+        if (error !== "User cancelled the operation") {
+            console.error('Error:', error);
+            alert('An error ocurred while updating appointments. Please Try Again.')
+        } else {
+            console.log('Operation Cacelled by the user.');
+        }
+
+    } finally {
+        clearSelectedCells();
+    }
 }
 
 
