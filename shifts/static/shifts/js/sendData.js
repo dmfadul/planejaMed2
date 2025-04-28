bcd87a81a02121b14ccbb5000fbd386127fc2ac4
dmfadul <dmf030@gmail.com>
2025-04-28 17:02:20 -0300

    moved getCookie to main static/js folder on its own file and called this file on base.html

--- shifts/static/shifts/js/sendData.js
+++ shifts/static/shifts/js/sendData.js
@@ -1,20 +1,3 @@
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.startsWith(name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-
 async function sendData() {
     try {
         await confirmData();
