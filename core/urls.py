7ebefe3887ed518d3e8a0702960a2c82868c70eb
doctormatterhorn <dmf030@gmail.com>
2025-05-24 23:26:12 -0300

    removed dashboard

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, dashboard
+from .views import CustomLoginView, CustomLogoutView
 
 app_name = "core"
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("dashboard", dashboard, name="dashboard"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,22 +1,17 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from django.views import View
+
 
 class CustomLoginView(LoginView):
     template_name = "core/login.html"
     redirect_authenticated_user = True  # Redirects already logged-in users
-    success_url = reverse_lazy("shifts:dashboard")  # Change this to the actual landing page
+    success_url = reverse_lazy("user_requests:calendar")  # Change this to the actual landing page
 
 
 class CustomLogoutView(View):
     def get(self, request):
         logout(request)
         return redirect(reverse_lazy("core:login"))
-
-@login_required
-def dashboard(request):
-    return render(request, "core/dashboard.html")
