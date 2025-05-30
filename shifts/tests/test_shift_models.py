bd5896fd8e469d8694803e2f0a406c5e11abbb52
doctormatterhorn <dmf030@gmail.com>
2025-05-30 10:03:26 -0300

    added more tests for converting to code

--- shifts/tests/test_shift_models.py
+++ shifts/tests/test_shift_models.py
@@ -21,7 +21,16 @@ class TestTemplateShift(TestCase):
             assert TemplateShift.convert_to_hours(key) == (start, end)
 
     def test_convert_to_code_exact_match(self):
+        assert TemplateShift.convert_to_code(7, 6) == "dn"
+        assert TemplateShift.convert_to_code(7, 1) == "dc"
+        assert TemplateShift.convert_to_code(7, 19) == "d"
+        assert TemplateShift.convert_to_code(19, 6) == "n"
         assert TemplateShift.convert_to_code(7, 13) == "m"
+        assert TemplateShift.convert_to_code(13, 19) == "t"
+        assert TemplateShift.convert_to_code(19, 1) == "c"
+        assert TemplateShift.convert_to_code(1, 6) == "v"
+        assert TemplateShift.convert_to_code(13, 6) == "tn"
+        assert TemplateShift.convert_to_code(13, 1) == "tc"
 
     def test_convert_to_code_partial(self):
         assert TemplateShift.convert_to_code(7, 12) == "d5"
