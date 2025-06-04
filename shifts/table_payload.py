97ee4091ba771cd374985de5409d027bc6b4bc85
doctormatterhorn <dmf030@gmail.com>
2025-06-03 21:27:59 -0300

    returned translate_to_table

--- shifts/table_payload.py
+++ shifts/table_payload.py
@@ -127,26 +127,3 @@ def add_shift(state, table_type):
         })
 
     return updates
-
-
-
-
-# def translate_to_table(shifts:list) -> dict:
-#     """Translate shifts to table formatting."""
-#     if not shifts:
-#         return {}
-    
-#     crm = shifts[0].user.crm
-#     shifts_per_day = defaultdict(list)
-#     for shift in shifts:
-#         shifts_per_day[(shift.weekday, shift.index)].append((shift.start_time, shift.end_time))
-
-#     output = {}
-#     for (weekday, week_index), hours in shifts_per_day.items():
-#         code = ""
-#         for hour in hours:
-#             code += TS.convert_to_code(*hour)
-        
-#         output[f"cell-{crm}-{weekday}-{week_index}"] = code
-    
-#     return output 
