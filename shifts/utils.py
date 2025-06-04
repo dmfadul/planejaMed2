e005ae52097272296fa92c3a442fcd692d6b59f0
doctormatterhorn <dmf030@gmail.com>
2025-06-03 21:13:39 -0300

    deleted translate to table old func from utils

--- shifts/utils.py
+++ shifts/utils.py
@@ -68,38 +68,3 @@ def gen_headers(template, month=None):
 
     return header1, header2
     
-
-def translate_to_table(shifts:list) -> dict:
-    """Translate a list of shifts from the same doctor to dictionary of cell-id keys and code values."""
-    if not shifts:
-        return {}
-    
-    crm = shifts[0].user.crm
-    shifts_per_day = defaultdict(list)
-    for shift in shifts:
-        if isinstance(shift, TS):
-            dict_key = (shift.weekday, shift.index)
-        else:
-            dict_key = shift.day
-
-        shifts_per_day[dict_key].append((shift.start_time, shift.end_time))
-
-    output = {}
-    for dict_key, hours in shifts_per_day.items():
-        code_lst = []
-        for hour in hours:
-            code_lst.append(TS.convert_to_code(*hour))
-        
-        code_order = SHIFT_CODES
-        order_map = {val: idx for idx, val in enumerate(code_order)}
-        code_lst.sort(key=lambda x: order_map.get(x, len(code_order)))
-        code = "".join(code_lst)
-
-        if isinstance(dict_key, tuple):
-            cell_id = f"cell-{crm}-{dict_key[0]}-{dict_key[1]}"
-        else:
-            cell_id = f"cell-{crm}-wday-{dict_key}"
-        
-        output[cell_id] = code
-    
-    return output   
