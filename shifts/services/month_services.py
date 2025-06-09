81a9f5e6cc49a499767bd8a5eafdf65f1db4dc75
doctormatterhorn <dmf030@gmail.com>
2025-06-09 18:05:16 -0300

    added bulk_create to month creation

--- shifts/services/month_services.py
+++ shifts/services/month_services.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from django.db import transaction
 from core.constants import END_DAY
 
 
@@ -8,33 +9,37 @@ def populate_month(month):
 
     year, num = month.year, month.number
     first_day = datetime(year, num, 1)
-    last_day = datetime(year, num, END_DAY)
     first_wday = first_day.weekday()
+    last_day = datetime(year, num, END_DAY)
 
     prv_year, prv_month = month.start_date.year, month.start_date.month
     prv_first_day = datetime(prv_year, prv_month, 1)
     prv_first_wday = prv_first_day.weekday()
 
     templates = TemplateShift.objects.filter(user__is_active=True)
+    shifts_to_create = []
 
-    for i, t in enumerate(templates):
+    for t in templates:
         day_offset_curr = (t.weekday - first_wday + 7) % 7
         day_offset_prv = (t.weekday - prv_first_wday + 7) % 7
 
         curr_date = first_day + timedelta(days=day_offset_curr) + timedelta(weeks=t.index - 1)
         prv_date = prv_first_day + timedelta(days=day_offset_prv) + timedelta(weeks=t.index - 1)
+        # Avoid 5th week shifts from following month having a prv_date on the current month
         if curr_date > last_day:
             continue
 
         for target_date in [curr_date, prv_date]:
             if month.start_date <= target_date <= month.end_date:
-                Shift.objects.create(
+                shifts_to_create.append(Shift(
                     user=t.user,
                     center=t.center,
                     month=month,
                     day=target_date.day,
                     start_time=t.start_time,
                     end_time=t.end_time
-                )
+                ))
+    with transaction.atomic():
+        Shift.objects.bulk_create(shifts_to_create)
 
-    print(f"Month {month} populated.")
+    print(f"Month {month} populated with {len(shifts_to_create)} shifts.")
