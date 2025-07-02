5feda0dd6678231387fb6ae94c7302a2ed97159b
doctormatterhorn <dmf030@gmail.com>
2025-07-02 19:39:11 -0300

    added calendar table

--- user_requests/views.py
+++ user_requests/views.py
@@ -1,17 +1,25 @@
 import logging
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from shifts.models import Month
+from shifts.models import Center, Month
 
 logger = logging.getLogger(__name__)
 
 
 @login_required
 def calendar(request, center_abbr):
+    center = get_object_or_404(Center, abbreviation=center_abbr)
     month = Month.objects.current()
     if not month:
         logger.error("No current month found.")
         return render(request, "user_requests/error.html", {"message": "No current month available."})
 
-    print(f"Current month: {month.name} ({month.year})")
-    return render(request, "user_requests/calendar.html")
+    calendar_data  = {
+        "open_center": center,
+        "month_number": month.number,
+        "month_name": month.name,
+        "year": month.year,
+        "table": month.gen_calendar_table(),
+    }
+    
+    return render(request, "user_requests/calendar.html", context=calendar_data)
