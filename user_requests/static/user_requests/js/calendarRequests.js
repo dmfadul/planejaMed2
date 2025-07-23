124f4145b3ce4e03e85f4adb4e86d3f6bfe341d9
doctormatterhorn <dmf030@gmail.com>
2025-07-23 14:04:32 -0300

    set up calendar js functions

--- user_requests/static/user_requests/js/calendarRequests.js
+++ user_requests/static/user_requests/js/calendarRequests.js
@@ -1,13 +1,40 @@
 function processCalRequest(crm, action) {
-    if(action === "include") {
+    const crmNumber = parseInt(crm, 10);
+    if (action === "include") {
+        handleCalInclude(crmNumber);
     } else if(action === "exclude") {
+        handleCalExclude(crmNumber);
     } else if(action === "donate") {
+        handleCalDonate(crmNumber);
     } else if(action === "exchange") {
+        handleCalExchange(crmNumber);
+    } else {
+        console.error("Unknown action: " + action);
     }
 }
 
+function handleCalInclude(crm) {
+}
 
-function includeCalRequest(crm) {
+function handleCalExclude(crm) {
+}
+
+function handleCalDonate(crm) {
+    console.log(typeof crm, typeof currentUserCrm)
+    if (crm === currentUserCrm) {
+        handleOfferingDonation(crm);
+    } else {
+        handleRequestingDonation(crm);
+    }
+}
+
+function handleOfferingDonation(crm) {
     let modal = new bootstrap.Modal(document.getElementById('modalRequests'));
     modal.show();
+}
+
+function handleRequestingDonation(crm) {
+}
+
+function handleCalExchange(crm) {
 }
