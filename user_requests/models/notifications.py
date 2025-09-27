eaf69129c559c350b17fab98154a453e6b2a17ab
doctormatterhorn <dmf030@gmail.com>
2025-09-26 22:42:21 -0300

    added imports to  notifications

--- user_requests/models/notifications.py
+++ user_requests/models/notifications.py
@@ -1,5 +1,7 @@
 from django.db import models
 from core.models import User
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
