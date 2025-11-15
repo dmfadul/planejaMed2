6726aa46ca2564c17aedf26cacae57c6ce5372aa
doctormatterhorn <dmf030@gmail.com>
2025-11-15 20:17:53 -0300

    removed duplicated database in settings

--- planejaMed2/settings.py
+++ planejaMed2/settings.py
@@ -96,18 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'planejaMed2.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -130,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
