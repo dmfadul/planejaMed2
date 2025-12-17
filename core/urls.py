afb2f10b25518b5d994f8e1501ca357c7648b631
doctormatterhorn <dmf030@gmail.com>
2025-12-17 18:21:10 -0300

    added a change password functionality to system

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 from .views import (
     CustomLoginView,
     CustomLogoutView,
@@ -17,4 +18,14 @@ urlpatterns = [
     path("profile/update/", ProfileUpdateView.as_view(), name="profile_update"),
     path("maintenance/", maintenance_notice, name="maintenance_notice"),
     path("maintenance/status/", maintenance_status, name="maintenance_status"),
+    path(
+        "password/change/",
+        auth_views.PasswordChangeView.as_view(template_name="core/password_change.html"),
+        name="password_change"
+    ),
+    path(
+        "password/change/done/",
+        auth_views.PasswordChangeDoneView.as_view(template_name="core/password_change_done.html"),
+        name="password_change_done"
+    ),
 ]
