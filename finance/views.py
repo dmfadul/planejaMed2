1a6ff8d55333895a329d2776016d01c3697e9dd2
doctormatterhorn <dmf030@gmail.com>
2026-03-15 00:01:31 -0300

    added upload structure

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UploadForm
 
-# Create your views here.
+
+def upload_view(request):
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload')
+    else:
+        form = UploadForm()
+    return render(request, 'finance/upload.html', {'form': form})
