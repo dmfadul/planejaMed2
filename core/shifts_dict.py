c04764b0c02a20c4a8d779950ca1c604be720fbd
doctormatterhorn <dmf030@gmail.com>
2026-03-17 23:13:01 -0300

    added shifts_dict to core and removed redudant alerts from finance dashboard

---
+++ core/shifts_dict.py
@@ -0,0 +1,68 @@
+staffing_hours = {
+    "CCG": {
+        "weekday": {
+            "morning": 54,
+            "afternoon": 54,
+            "night": 36,
+        },
+        "saturday": {
+            "morning": 42,
+            "afternoon": 36,
+            "night": 24,
+        },
+        "sunday": {
+            "morning": 36,
+            "afternoon": 36,
+            "night": 24,
+        },
+        "holiday": {
+            "morning": 24,
+            "afternoon": 24,
+            "night": 24,
+        },
+    },
+    "CCQ": {
+        "weekday": {
+            "morning": 18,
+            "afternoon": 18,
+            "night": 0,
+        },
+    },
+    "CCO": {
+        "weekday": {
+            "morning": 12,
+            "afternoon": 12,
+            "night": 12,
+        },
+        "saturday": {
+            "morning": 12,
+            "afternoon": 12,
+            "night": 12,
+
+        },
+        "sunday": {
+            "morning": 12,
+            "afternoon": 12,
+            "night": 12,
+        },
+        "holiday": {
+            "morning": 12,
+            "afternoon": 12,
+            "night": 12,
+        },
+    },
+    "SADT": {
+        "all": {
+            "morning": 6,
+            "afternoon": 6,
+            "night": 0,
+        },
+    },
+    "ECO": {
+        "all": {
+            "morning": 0,
+            "afternoon": 0,
+            "night": 0,
+        },
+    },
+}
