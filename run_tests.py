ffa7d5051ad33d636b020317f1b3d3d7b28277a7
doctormatterhorn <dmf030@gmail.com>
2026-06-29 22:55:30 -0300

    added all TODOs

--- run_tests.py
+++ run_tests.py
@@ -23,13 +23,42 @@ print("User:", user.compliant_since)
 
 
 # FINAL TASKS:
-
 # TODO: add tests
+# TODO: check if admin/root distictions are working properly
+# TODO: add a teamLeader group and give them the ability to authorize inclusions
+
+# VACATIONS:
+# TODO: Fix (?) vacations
+# TODO: Remove urgency hours from vacation payment calculation. (only routine hours should be considered)
 # TODO: add alert (have to keep following the vacation compliance rules) to vacation authorization message.
+
+# VACATION COMPLIANCE:
+# TODO: do not enforce vacation rules on the month when user is on vacation
+# TODO: add a compliance check:
+# month creation -- check for base rights
+# unlock month -- check for month rights
+# TODO: check donation text ("de" and "para" seems to be confusing users)
+
+# SHIFTS:
+# TODO: fix names links (the redirects are not working properly in all kinds of tables)
 # TODO: fix bug when creating/unlocking month from users hours view.
 # TODO: there are incorrect hours (ex: d: 07:00 - 13:00) in hours selection menu.
-# TODO: Remove urgency hours from vacation payment calculation. (only routine hours should be considered)
+# TODO: fix bug when edit button is clicked when opening a table
+# TODO: remove the edit button from sumDoctor table
+# TODO: add visible month/year to both the new month and unlock month modal dialogs.
+# TODO: check if there is a difference between original and realized months
+# TODO: finish report
+
+# USER REQUESTS:
+# TODO: user requests for a future month should be allowed
+# TODO: Open requests (requests that are not for an specific user, but can be accepted by anyone)
+
+# FINANCE:
+# TODO: individual view of finance data (for each user)
+# TODO: finance reports
 
 # POSSIBLE FUTURE TASKS:
+# normalize modal themes (make them all the same)
+# reorganize the admin functions (make them more intuitive)
 # add an automatic email notification to users when their compliance status changes.
 # add an automatic password reset email functionality.
