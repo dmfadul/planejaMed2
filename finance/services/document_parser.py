85ad73378cc83257d6018aa5d4a7839e45d52aac
doctormatterhorn <dmf030@gmail.com>
2026-07-20 22:15:47 -0300

    updated requirements (to include pandas and xlrd) and adde a temp content to process_uploaded_document()

--- finance/services/document_parser.py
+++ finance/services/document_parser.py
@@ -1,11 +1,18 @@
-import pandas as pd
+import xlrd
 from finance.models import UploadedDocument
 
 
 def process_uploaded_document(document: UploadedDocument) -> None:
     file_path = document.file.path
-    dataframe = pandas.read_excel(file_path)
+
+    workbook = xlrd.open_workbook(document.file.path)
+    sheet = workbook.sheet_by_index(0)
+
+    for row_index in range(sheet.nrows):
+        row = sheet.row_values(row_index)
+        print(row)
 
     # Example:
+    # dataframe = pandas.read_excel(file_path)
     # validate_document(dataframe)
     # save_document_data(document, dataframe)
