b699150a191aaba7763a8fd75d7b89c52e6ed7fd
doctormatterhorn <dmf030@gmail.com>
2026-07-27 22:49:55 -0300

    added key_payment dict

---
+++ finance/key_payment_dict.py
@@ -0,0 +1,20 @@
+KEY_PAYMENT_DICT = {
+    "consulta particular" : [
+        "AVALIACAO PRE-ANESTESICA"
+    ],
+    "consulta convenios": [
+        "CONSULTA COM ANESTESIOLOGISTA",
+    ],
+    "consulta sus": [
+        "CONSULTA/AVALIAÇÃO EM PACIENTE INTERNADO",
+        "CONSULTA MEDICA EM ATENÇÃO ESPECIALIZADA",
+        "SUS AMBULAT/URGENCIA/EXTERNO",
+        "SUS - INTERNACAO"
+    ]
+}
+
+KEY_ENSURANCE_DICT = {
+    "consulta particular" : [
+        "PARTICULAR"
+    ]
+}
