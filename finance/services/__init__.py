41b640956ee46313ed7a51a44a556e8b8f4046a4
doctormatterhorn <dmf030@gmail.com>
2026-08-01 10:18:56 -0300

    added create_original_batch import

--- finance/services/__init__.py
+++ finance/services/__init__.py
@@ -5,3 +5,5 @@ from .grids_builder import (
 )
 
 from .document_parser import process_uploaded_document
+from .hospital_import import create_original_batch
+# from .cooperative_export import export_cooperative_financial_data
