060f05792df45551db5117df0509c5267cda4f0b
doctormatterhorn <dmf030@gmail.com>
2026-08-06 20:00:10 -0300

    added admin settings to show users in group and migrate

--- core/admin.py
+++ core/admin.py
@@ -5,9 +5,45 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import Group
+from django.utils.html import format_html_join
 
 User = get_user_model()
 
+admin.site.unregister(Group)
+
+
+@admin.register(Group)
+class CustomGroupAdmin(GroupAdmin):
+    readonly_fields = ("member_list",)
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                "name",
+                "permissions",
+                "member_list",
+            )
+        }),
+    )
+
+    @admin.display(description="Members")
+    def member_list(self, group):
+        if not group.pk:
+            return "Save the group before adding members."
+
+        users = User.objects.filter(groups=group).order_by("name")
+
+        if not users.exists():
+            return "No members in this group."
+
+        return format_html_join(
+            "",
+            "<div>{}</div>",
+            ((user.name,) for user in users),
+        )
+    
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
